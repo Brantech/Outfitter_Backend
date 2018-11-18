f6ad7a3e651f27702e3d94d7f95bb905f152e7c5
Daquaris Chadwick <sourhead5@gmail.com>
2018-11-18 04:29:28 -0500

    created test model for clothing classifier

--- core/learn/main.py
+++ core/learn/main.py
@@ -14,6 +14,10 @@ def main():
     fashion_mnist = keras.datasets.fashion_mnist
 
     (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    
+    # rescale values from 0-255 to 0-1
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
 
     class_names = [
         'T-shirt/top', 
@@ -25,11 +29,36 @@ def main():
         'Bag',
         'Ankle boot']
 
-    plt.figure()
-    plt.imshow(train_images[0])
-    plt.colorbar()
-    plt.grid(False)
+    # plt.figure(figsize=(10,10))
+    
+    '''
+    for i in range(25):
+        plt.subplot(5, 5, i+1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(train_images[0])
+        plt.imshow(train_images[i], cmap=plt.cm.binary)
+    
     plt.show()
+    '''
+
+    # Create the model
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy']
+    )
+
+    model.fit(train_images, train_labels, epochs=5)
+
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+    print('Test Accuracy: ', test_acc)
 
 print('================> START')
 main()
