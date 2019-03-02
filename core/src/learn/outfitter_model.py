98db9f450397f357f7226002568f211c80c9cf67
Daquaris Chadwick <sourhead5@gmail.com>
2019-03-02 10:06:04 -0500

    OFR-49: translated keras tutorial to tensorflow based

--- core/src/learn/outfitter_model.py
+++ core/src/learn/outfitter_model.py
@@ -8,64 +8,133 @@ The core learning class.
 
 '''
 # TensorFlow and tf.keras
-import tensorflow as tf
-from tensorflow import keras
-from keras.models import load_model
-
-# Helper Libraries
-import numpy as np
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.layers import Flatten
+import numpy as np
+import pandas as pd
 
+def parse_labels(labels, label_count=10):
+    ret_array = []
+    for label in labels:
+        out = np.zeros((label_count,))
+        out[label] = 1
+        ret_array.append(out)
+    return np.array(ret_array)
 
 class OutfitterModel:
+    def __init__(self, data, classes):
+        (self.train_images, self.train_labels) = data
 
-    def __init__(self):
-        self.class_names = [
-            'T-shirt/top', 
-            'Trouser',
-            'Pullover',
-            'Dress',
-            'Coat',
-            'Sandal',
-            'Shirt',
-            'Sneaker',
-            'Bag',
-            'Ankle boot']
+        self.n_class = len(classes)
 
-        self.fashion_mnist = keras.datasets.fashion_mnist
-        (self.train_images, self.train_labels), (self.test_images, self.test_labels) = self.fashion_mnist.load_data()
-
+        # Preprocessing
         self.train_images = self.train_images / 255.0
-        self.test_images = self.test_images / 255.0
-
-
-    def compile_model(self):
-        self.model.compile(optimizer=tf.train.AdamOptimizer(),
-            loss='sparse_categorical_crossentropy',
-            metrics=['accuracy']
-        )
-
-
-    def train(self, path, train_input, train_output):
-        ''' Train predictor '''
-
-        # Create the model
-        self.model = keras.Sequential([
-            keras.layers.Flatten(input_shape=(28, 28)),
-            keras.layers.Dense(128, activation=tf.nn.relu),
-            keras.layers.Dense(10, activation=tf.nn.softmax)
-        ])
-
-        self.compile_model()
-
-        self.model.fit(train_input, train_output, epochs=5)
-        #self.model.save(path)
-    
-    def predict(self, test_data):
-        test_image, test_label = test_data
+        self.train_labels = parse_labels(self.train_labels, self.n_class)
         
-        test_image = (np.expand_dims(test_image, 0))
-        prediction = self.model.predict(test_image)
-        pred = np.argmax(prediction[0])
+        self.learning_rate = 0.01
+        self.training_epochs = 1000
+        self.n_dim = self.train_images.shape[1:]
+        
+        self.train_images = self.train_images.reshape(self.train_images.shape[0], -1)
 
-        print('corr: %s - pred: %s'%(self.class_names[test_label], self.class_names[pred]))
+        self.n_hidden_1 = 128
+        self.n_hidden_2 = self.n_class
+
+        self.weights = {
+            'h1': tf.Variable(tf.truncated_normal([self.n_dim[0] * self.n_dim[1], self.n_hidden_1])),
+            'h2': tf.Variable(tf.truncated_normal([self.n_hidden_1, self.n_hidden_2]))
+        }
+        self.biases = {
+            'b1': tf.Variable(tf.truncated_normal([self.n_hidden_1])),
+            'b2': tf.Variable(tf.truncated_normal([self.n_hidden_2]))
+        }
+
+    def train(self, test_input, test_output):
+        ''' Train predictor '''
+        
+        test_input = test_input.reshape(test_input.shape[0], -1)
+        test_output = parse_labels(test_output, self.n_class)
+
+        x = tf.placeholder(tf.float32, [None, self.n_dim[0] * self.n_dim[1]]) # Flatten this
+        W = tf.Variable(tf.zeros([(self.n_dim[0] * self.n_dim[1]), self.n_class]))
+        b = tf.Variable(tf.zeros([self.n_class]))
+        y_ = tf.placeholder(tf.float32, [None, self.n_class])
+        
+        y = self.multilayer_perceptron(x, self.weights, self.biases)
+        
+        saver = tf.train.Saver()
+
+        cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=y, labels=y_))
+        training_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cost_function)
+
+        init = tf.global_variables_initializer()
+
+        mse_history = []
+        accuracy_history = []
+        cost_history = []
+
+        with tf.Session() as sess:
+            sess.run(init)
+            for epoch in range(self.training_epochs):
+                sess.run(training_step, feed_dict={x: self.train_images, y_: self.train_labels})
+                cost = sess.run(cost_function, feed_dict={x: self.train_images, y_: self.train_labels})
+                cost_history = np.append(cost_history, cost)
+                correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+                accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+                pred_y = sess.run(y, feed_dict={x: test_input})
+                mse = tf.reduce_mean(tf.square(pred_y - test_output))
+                mse_ = sess.run(mse)
+                mse_history.append(mse_)
+                accuracy = (sess.run(accuracy, feed_dict={x: self.train_images, y_: self.train_labels}))
+                accuracy_history.append(accuracy)
+
+                print('epoch: ', epoch, ' - ', 'cost: ', cost, ' - MSE: ', mse_, ' - Train Accuracy: ', accuracy)
+
+            #save_path = saver.save(sess, model_path)
+            plt.plot(mse_history, 'r')
+            plt.show()
+            plt.plot(accuracy_history)
+            plt.show()
+
+            correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            print("Test Accuracy: ", (sess.run(accuracy, feed_dict={x: test_input, y_: test_output})))
+
+            pred_y = sess.run(y, feed_dict={x: test_input})
+            mse = tf.reduce_mean(tf.square(pred_y - test_output))
+            print("MSE: %.4f"%sess.run(mse))
+        # # Create the model
+        # self.model = keras.Sequential([
+        #     keras.layers.Flatten(input_shape=(28, 28)),
+        #     keras.layers.Dense(128, activation=tf.nn.relu),
+        #     keras.layers.Dense(10, activation=tf.nn.softmax)
+        # ])
+
+        # self.compile_model()
+
+        # self.model.fit(train_input, train_output, epochs=5)
+        # #self.model.save(path)
+
+    def multilayer_perceptron(self, x, weights, biases):
+        layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+        layer_1 = tf.nn.relu(layer_1)
+
+        out_layer = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+        out_layer = tf.nn.softmax(out_layer)
+        return out_layer
+
+    # def compile_model(self):
+    #     self.model.compile(optimizer=tf.train.AdamOptimizer(),
+    #         loss='sparse_categorical_crossentropy',
+    #         metrics=['accuracy']
+    #     )
+    
+    # def predict(self, test_data):
+    #     test_image, test_label = test_data
+        
+    #     test_image = (np.expand_dims(test_image, 0))
+    #     prediction = self.model.predict(test_image)
+    #     pred = np.argmax(prediction[0])
+
+    #     print('corr: %s - pred: %s'%(self.class_names[test_label], self.class_names[pred]))
--- core/src/main.py
+++ core/src/main.py
@@ -5,48 +5,32 @@
     
     The main entry point
 '''
-import matplotlib.pyplot as plt
-
-import keras
-import learn.outfitter_model as OModel
-import math
-import numpy as np
-import os
+from learn.outfitter_model import OutfitterModel
 import tensorflow as tf
-
-from keras.utils import CustomObjectScope
-from keras.initializers import glorot_uniform
-from keras.models import load_model 
-
-from skimage import color, img_as_float
-from skimage.transform import resize
-from skimage.util import pad
+import numpy as np
+from tensorflow import keras
 
 
-data_path = os.path.abspath('./data')
+class_names = [
+            'T-shirt/top', 
+            'Trouser',
+            'Pullover',
+            'Dress',
+            'Coat',
+            'Sandal',
+            'Shirt',
+            'Sneaker',
+            'Bag',
+            'Ankle boot']
 
-def parse_image(filename, label):
-    imar = np.array([[1.,0.], [0.,0.],
-                    [0.,1.], [0.,1.],
-                    [0.,0.], [1.,1.]]).transpose()
-    im = plt.imread(filename)
-    im = img_as_float(im)
-    im = color.rgb2gray(im)
 
-    diff = int((len(im)-len(im[0])))
-    rem = diff % 2
-    padding_count = math.ceil(diff/2)
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
-    padding = tf.constant([[rem, 0], [padding_count, padding_count]])
-    im = tf.pad(tf.constant(im), padding, mode="constant", constant_values=1)
-    im = tf.Session().run(im)
-    
-    im = resize(im, (28, 28), anti_aliasing=True)
+test_images = test_images / 255.0
+# list_of_lists = [train_images.shape[1:], 128]
+# print([len(list_of_lists)] + [len(list_of_lists[0])])
+# exit()
 
-    return im, label
-
-model = OModel.OutfitterModel()
-model.train(os.path.join(data_path, 'clothing_pred.h5'), model.train_images, model.train_labels)
-
-x = parse_image(os.path.join(data_path, 'shirt.jpg'), 0)
-model.predict(x)
+model = OutfitterModel((train_images, train_labels), class_names)
+model.train(test_images, test_labels)
