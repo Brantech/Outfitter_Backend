2962dcaa383dad135c2db01c644b488ab50b5d27
Daquaris Chadwick <sourhead5@gmail.com>
2019-03-03 01:58:39 -0500

    OFR-49: added code for resnet and mp

--- core/src/learn/outfitter_model.py
+++ core/src/learn/outfitter_model.py
@@ -13,6 +13,11 @@ MAKE THIS FASTER!!!!
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.layers import Flatten
+from keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from keras.layers import Dense, Dropout, Activation
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.preprocessing import image
 import numpy as np
 import pandas as pd
 
@@ -25,93 +30,49 @@ def parse_labels(labels, label_count=10):
     return np.array(ret_array)
 
 class OutfitterModel:
-    def __init__(self, data, classes):
-        (self.train_images, self.train_labels) = data
-
-        self.n_class = len(classes)
-
-        # Preprocessing
-        self.train_images = self.train_images / 255.0
-        self.train_labels = parse_labels(self.train_labels, self.n_class)
+    def get_features(self, x):
+        # Feature Extractor - Test other feature extractors
+        base_model = ResNet50(weights='imagenet', include_top=False)
         
-        self.learning_rate = 0.01
-        self.training_epochs = 1000
-        self.n_dim = self.train_images.shape[1:]
-        
-        self.train_images = self.train_images.reshape(self.train_images.shape[0], -1)
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)
+        return base_model.predict(x)
+    
+    def process_outfit(self, images):
+        feature_vector_final = None
+        for image in images:
+            image_feature_vector = self.get_features(image).flatten()
+            if feature_vector_final is None:
+                feature_vector_final = image_feature_vector
+            else:
+                feature_vector_final = np.concatenate((feature_vector_final, image_feature_vector), axis=None)
+        return feature_vector_final
 
-        self.n_hidden_1 = 128
-        self.n_hidden_2 = self.n_class
+    def create_multilayer_perceptron(self, input_layer_shape, class_count):
+        model = Sequential()
 
-        self.weights = {
-            'h1': tf.Variable(tf.truncated_normal([self.n_dim[0] * self.n_dim[1], self.n_hidden_1])),
-            'h2': tf.Variable(tf.truncated_normal([self.n_hidden_1, self.n_hidden_2]))
-        }
-        self.biases = {
-            'b1': tf.Variable(tf.truncated_normal([self.n_hidden_1])),
-            'b2': tf.Variable(tf.truncated_normal([self.n_hidden_2]))
-        }
+        model.add(Dense(64, activation='relu', input_shape=input_layer_shape))
+        model.add(Dropout(0.5))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(class_count, activation='softmax'))
+        return model
 
-    def train(self, test_input, test_output):
-        ''' Train predictor '''
-        
-        test_input = test_input.reshape(test_input.shape[0], -1)
-        test_output = parse_labels(test_output, self.n_class)
+    @staticmethod
+    def train(train_input, train_output, test_data, classes=[1, 2, 3, 4, 5]):
+        outfitModel = OutfitterModel()
+        (test_in, test_out) = test_data
 
-        x = tf.placeholder(tf.float32, [None, self.n_dim[0] * self.n_dim[1]]) # Flatten this
-        W = tf.Variable(tf.zeros([(self.n_dim[0] * self.n_dim[1]), self.n_class]))
-        b = tf.Variable(tf.zeros([self.n_class]))
-        y_ = tf.placeholder(tf.float32, [None, self.n_class])
-        
-        y = self.multilayer_perceptron(x, self.weights, self.biases)
-        
-        saver = tf.train.Saver()
+        final_feature_vector = []
+        for outfit in train_input:
+            final_feature_vector = np.concatenate((final_feature_vector, outfitModel.process_outfit(outfit)))
 
-        cost_function = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=y, labels=y_))
-        training_step = tf.train.AdamOptimizer(self.learning_rate).minimize(cost_function)
+        model = outfitModel.create_multilayer_perceptron(final_feature_vector.shape, len(classes))
+        sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        model.compile(loss='categorical_crossentropy',
+                    optimizer=sgd,
+                    metrics=['accuracy'])
+        model.fit(final_feature_vector, np.asarray(train_output),  epochs=5, batch_size=1)
 
-        init = tf.global_variables_initializer()
-
-        mse_history = []
-        accuracy_history = []
-        cost_history = []
-
-        with tf.Session() as sess:
-            sess.run(init)
-            for epoch in range(self.training_epochs):
-                sess.run(training_step, feed_dict={x: self.train_images, y_: self.train_labels})
-                cost = sess.run(cost_function, feed_dict={x: self.train_images, y_: self.train_labels})
-                cost_history = np.append(cost_history, cost)
-                correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-                accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-                pred_y = sess.run(y, feed_dict={x: test_input})
-                mse = tf.reduce_mean(tf.square(pred_y - test_output))
-                mse_ = sess.run(mse)
-                mse_history.append(mse_)
-                accuracy = (sess.run(accuracy, feed_dict={x: self.train_images, y_: self.train_labels}))
-                accuracy_history.append(accuracy)
-
-                if epoch % 50 == 0:
-                    print('epoch: ', epoch, ' - ', 'cost: ', cost, ' - MSE: ', mse_, ' - Train Accuracy: ', accuracy)
-
-            #save_path = saver.save(sess, model_path)
-            plt.plot(mse_history, 'r')
-            plt.show()
-            plt.plot(accuracy_history)
-            plt.show()
-
-            correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-            print("Test Accuracy: ", (sess.run(accuracy, feed_dict={x: test_input, y_: test_output})))
-
-            pred_y = sess.run(y, feed_dict={x: test_input})
-            mse = tf.reduce_mean(tf.square(pred_y - test_output))
-            print("MSE: %.4f"%sess.run(mse))
-
-    def multilayer_perceptron(self, x, weights, biases):
-        layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
-        layer_1 = tf.nn.relu(layer_1)
-
-        out_layer = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
-        out_layer = tf.nn.softmax(out_layer)
-        return out_layer
+        test = model.evaluate(test_in, test_out)
+        print(test)
--- core/src/main.py
+++ core/src/main.py
@@ -6,28 +6,68 @@
     The main entry point
 '''
 from learn.outfitter_model import OutfitterModel
-import tensorflow as tf
+from keras.preprocessing import image
 import numpy as np
-from tensorflow import keras
+import os
+import re
 
+def get_images(root_dir):
+    result = {'tops': [], 'bottoms': []}
+    for parent, _, files in os.walk(root_dir):
+        for filename in files:
+            if not filename.endswith(('.jpg', '.png')):
+                continue
+            path = os.path.join(parent, filename)
+            match = re.match(r'.*/(tops|bottoms).*', path)
+            if not match:
+                continue
+            clothing = match.groups()[0]
+            result[clothing].append((path, filename))
+    return result
 
-class_names = [
-            'T-shirt/top', 
-            'Trouser',
-            'Pullover',
-            'Dress',
-            'Coat',
-            'Sandal',
-            'Shirt',
-            'Sneaker',
-            'Bag',
-            'Ankle boot']
+clothing_items = get_images('data/')
 
+train_input = []
+dataset_num = 2
+for i in range(dataset_num):
 
-fashion_mnist = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    url_top = clothing_items['tops'][i][0]
+    url_bottom = clothing_items['bottoms'][i][0]
 
-test_images = test_images / 255.0
+    top = image.load_img(url_top, target_size=(224, 224))
+    top = image.img_to_array(top)
 
-model = OutfitterModel((train_images, train_labels), class_names)
-model.train(test_images, test_labels)
+    bottom = image.load_img(url_bottom, target_size=(224, 224))
+    bottom = image.img_to_array(bottom)
+
+    outfit = [top, bottom]
+    train_input.append(outfit)
+
+inp = [3, 1, 4, 5, 1]
+train_output = []
+for i in range(dataset_num):
+
+    train_output.append(np.zeros((5,)))
+    train_output[i][inp[i] - 1] = 1
+
+test_input = []
+
+url_top = clothing_items['tops'][5][0]
+url_bottom = clothing_items['bottoms'][5][0]
+
+top = image.load_img(url_top, target_size=(224, 224))
+top = image.img_to_array(top)
+
+bottom = image.load_img(url_bottom, target_size=(224, 224))
+bottom = image.img_to_array(bottom)
+
+outfit = [top, bottom]
+test_input.append(outfit)
+
+test_output = []
+test_output.append(np.zeros((5,)))
+test_output[0][3] = 1
+
+OutfitterModel.train(train_input, train_output, (train_input, train_output))
+
+#print(train_input, train_output, test_input, test_output)
