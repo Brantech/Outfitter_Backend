b85d99ae36d71851ce5a63717c14f4eba8f0a85f
Kyle Martinez <git@kylem.art>
2019-04-09 15:07:39 -0400

    Fixed api router

--- routes/api.js
+++ routes/api.js
@@ -1,12 +1,12 @@
 var express = require('express');
-var garments = require('./api/garments.route');
-var surveys = require('./api/surveys.route');
+var garments = require('./api/garment.route');
+var surveys = require('./api/survey.route');
 var users = require('./api/user.route');
 
 var router = express.Router();
 
 router.use('/garments', garments);
-router.use('/survey', surveys);
+router.use('/surveys', surveys);
 router.use('/users', users);
 
 module.exports = router;
