8a78aae9a42ba15160fb8815bc0020ffac007465
Kyle Martinez <git@kylem.art>
2019-04-09 12:48:02 -0400

    All fields are required by survey model

--- models/Survey.model.js
+++ models/Survey.model.js
@@ -2,33 +2,61 @@ var mongoose = require('mongoose');
 var isUrl = require('./validators/url.validator');
 
 var SurveySchema = new mongoose.Schema({
-    sex: Number,
-    state: Number,
-    formality: Number,
-    weather: Number,
-    temperature: Number,
-    season: Number,
+    sex: {
+        type: Number,
+        required: true
+    },
+    state: {
+        type: Number,
+        required: true
+    },
+    formality: {
+        type: Number,
+        required: true
+    },
+    weather: {
+        type: Number,
+        required: true
+    },
+    temperature: {
+        type: Number,
+        required: true
+    },
+    season: {
+        type: Number,
+        required: true
+    },
     createdOutfit: {
         top: {
             type: String,
+            required: true,
             validate: isUrl
         },
         bottom: {
             type: String,
+            required: true,
             validate: isUrl
         },
-        rating: Number
+        rating: {
+            type: Number,
+            required: true
+        }
     },
     randomOutfit: {
         top: {
             type: String,
+            required: true,
             validate: isUrl
         },
         bottom: {
             type: String,
+            required: true,
             validate: isUrl
         },
-        rating: Number
+        rating: {
+            type: Number,
+            required: true
+        }
     }
 });
 
