75fd0462d9ddd1743f66b4b26e32df40e5c2f864
Kyle Martinez <git@kylem.art>
2019-04-09 15:25:17 -0400

    url validator finalized

--- models/validators/url.validator.js
+++ models/validators/url.validator.js
@@ -1,12 +1,11 @@
 var validate = require('mongoose-validator');
 
-var isUrl = {
-    validator: value => validate.isURL(value, { 
-        protocols: ['http','https','ftp'], 
-        require_tld: true, 
-        require_protocol: true
-    }),
+var isURL = validate({
+    validator: 'isURL',
+    protocols: ['http','https','ftp'], 
+    require_tld: true, 
+    require_protocol: true,
     message: 'invalid url scheme'
-};
+});
 
-module.exports = isUrl;
+module.exports = isURL;
